cd350566e3d2e0c46b3e6a0b8a16de7e0b3f0dec
Martino-Pistellato <smart.piste@gmail.com>
2022-12-10 17:02:59 +0100

    completamento bottom bar e creazione attività

--- app/src/main/java/com/example/bekind_v2/UILayer/BottomBar.java
+++ app/src/main/java/com/example/bekind_v2/UILayer/BottomBar.java
@@ -1,8 +1,12 @@
 package com.example.bekind_v2.UILayer;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.example.bekind_v2.R;
 import com.example.bekind_v2.databinding.ActivityBottomBarBinding;
@@ -13,19 +17,29 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.lifecycle.ViewModelProvider;
 
-import com.example.bekind_v2.databinding.ActivityBottomBarBinding;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class BottomBar extends AppCompatActivity {
 
     private ActivityBottomBarBinding binding;
+    private BottomBarViewModel bottomBarViewModel;
     private Button addProposalButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityBottomBarBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        bottomBarViewModel = new ViewModelProvider(this).get(BottomBarViewModel.class);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -41,9 +55,77 @@ public class BottomBar extends AppCompatActivity {
         addProposalButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Dialog dialog = new Dialog(getApplicationContext()); //TODO: blend declaration and initialization
+                TextInputEditText title, body;
+                DatePicker expiringDate;
+                TimePicker expiringHour;
+                Chip shoppingChip, houseworksChip, cleaningChip, transportChip, randomChip;
+                Button closeBtn, publishBtn;
 
+                dialog.setContentView(R.layout.add_proposal_popup); //set content of dialog (look in layout folder for new_activity_dialog file)
+                dialog.setCanceledOnTouchOutside(false); //prevents dialog to close when clicking outside of it
+
+                shoppingChip = dialog.findViewById(R.id.shopping_chip_popup);
+                houseworksChip = dialog.findViewById(R.id.houseworks_chip_popup);
+                cleaningChip = dialog.findViewById(R.id.cleaning_chip_popup);
+                transportChip = dialog.findViewById(R.id.transport_chip_popup);
+                randomChip = dialog.findViewById(R.id.random_chip_popup);
+
+                shoppingChip.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) { bottomBarViewModel.manageFilter(shoppingChip.getText().toString()); }
+                });
+                houseworksChip.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) { bottomBarViewModel.manageFilter(houseworksChip.getText().toString()); }
+                });
+                cleaningChip.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) { bottomBarViewModel.manageFilter(cleaningChip.getText().toString()); }
+                });
+                transportChip.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) { bottomBarViewModel.manageFilter(transportChip.getText().toString()); }
+                });
+                randomChip.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) { bottomBarViewModel.manageFilter(randomChip.getText().toString()); }
+                });
+
+                closeBtn = dialog.findViewById(R.id.close_btn); //button for closing dialog
+                publishBtn = dialog.findViewById(R.id.publish_btn); //button to confirm and publish activity
+
+                title = dialog.findViewById(R.id.activity_title);
+                body = dialog.findViewById(R.id.activity_body);
+                expiringDate = dialog.findViewById(R.id.date_picker);
+                expiringHour = dialog.findViewById(R.id.time_picker);
+
+                bottomBarViewModel.setExpiringHour(expiringHour);
+                bottomBarViewModel.setExpiringDate(expiringDate);
+
+                closeBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss(); //close dialog
+                    }
+                });
+
+                publishBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Date proposalExpiringDate = bottomBarViewModel.toDate(expiringDate.getYear(), expiringDate.getMonth(), expiringDate.getDayOfMonth(), expiringHour.getHour(), expiringHour.getMinute());
+                        String proposalTitle = title.getText().toString().trim(); //gets the content of the title
+                        String proposalBody = body.getText().toString().trim(); //gets the content of the body
+
+                        if(!bottomBarViewModel.checkConstraints(title, proposalTitle, body, proposalBody, proposalExpiringDate))
+                            Toast.makeText(getApplicationContext(), "Errore: i campi non sono stati riempiti correttamente", Toast.LENGTH_SHORT).show();
+                        else //all the parameters in input are correct
+                            bottomBarViewModel.createProposal(proposalTitle, proposalBody, proposalExpiringDate);
+                    }
+                });
+
+                dialog.show();
             }
         });
     }
-
 }
---
+++ app/src/main/java/com/example/bekind_v2/UILayer/BottomBarViewModel.java
@@ -0,0 +1,76 @@
+package com.example.bekind_v2.UILayer;
+
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import androidx.lifecycle.ViewModel;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.ArrayList;
+
+import com.example.bekind_v2.DataLayer.ProposalRepository;
+import com.example.bekind_v2.DataLayer.UserManager;
+
+public class BottomBarViewModel extends ViewModel {
+    private ArrayList<String> filters;
+
+    public BottomBarViewModel() {
+        filters = new ArrayList<>();
+    }
+
+    public void createProposal(String title, String body, Date expiringDate){
+        String userId = UserManager.getUserId();
+        ProposalRepository.createProposal(title, body, expiringDate, userId, null, filters);
+    }
+
+    public Date toDate(int year, int month, int day, int hour, int minute){
+        Calendar expiringDate = Calendar.getInstance();
+        expiringDate.set(year, month, day, hour, minute);
+        return expiringDate.getTime();
+    }
+
+    public void manageFilter(String filter){
+        if(filters.contains(filter))
+            filters.remove(filter);
+        else
+            filters.add(filter);
+    }
+
+    public void setExpiringHour(TimePicker expiringHour){
+        //by default, we set time in timepicker to the hour and minute when the add button is clicked (+1 because of LocalDateTime implementation)
+        expiringHour.setIs24HourView(true);  //time picker use 24h format
+        expiringHour.setHour(LocalDateTime.now().getHour() + 1);
+        expiringHour.setMinute(LocalDateTime.now().getMinute());
+    }
+
+    public void setExpiringDate(DatePicker expiringDate){
+        //by default, we set date in datepicker to today when the add button is clicked (+1/-1 because of LocalDateTime implementation)
+        expiringDate.updateDate(LocalDateTime.now().getYear() - 1, LocalDateTime.now().getMonthValue() + 1, LocalDateTime.now().getDayOfMonth());
+        //we cannot create an activity with an expiringDate that comes before the moment we clicked the add activity button (-1000 (one second) cause we cannot use the EXACT moment)
+        expiringDate.setMinDate(System.currentTimeMillis() - 1000);
+    }
+
+    public boolean checkDateConstraint(Date expiringDate){
+        LocalDateTime current = LocalDateTime.now();
+        Date currentDate = toDate(current.getYear(), current.getMonthValue(), current.getDayOfMonth(), current.getHour(), current.getMinute());
+        return expiringDate.after(currentDate);
+    }
+
+    public boolean checkConstraints(TextInputEditText title, String proposalTitle, TextInputEditText body, String proposalBody, Date proposalExpiringDate){
+        if(proposalTitle.isEmpty()){ //checks if the title is empty, in that case it blocks the creation until it is filled
+            title.setError("Questo campo non può essere vuoto");
+            title.requestFocus();
+            return false;
+        }
+        else if(proposalBody.isEmpty()){ //checks if the body is empty, in that case it blocks the creation until it is filled
+            body.setError("Questo campo non può essere vuoto");
+            body.requestFocus();
+            return false;
+        }
+        else return checkDateConstraint(proposalExpiringDate);
+    }
+}
